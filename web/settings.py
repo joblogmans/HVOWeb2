d489232c80c0cd46994465bb80ab7ab99bcb5b5f
Job Logmans <job.logmans@kreativsoftware.com>
2022-12-30 13:23:51 +0000

    Added login and logout pages

--- web/settings.py
+++ web/settings.py
@@ -128,5 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = "home"
-LOGOUT_REDIRECT_URL = "home"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
